0288761851740de62585f32b2c7557cda7daac6e
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-01-31 11:49:42 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var http = require('http');
+var fs = require("fs");
+
+var server = http.createServer();
+server.on('request', function (request, response) {
+
+    if (request.method === 'GET' && request.url === '/') {
+        response.setHeader("Content-Type", "text/html; charset=utf-8");
+        fs.readFile('./index.html', function (err, data) {
+            response.write(data);
+            response.end();
+        })
+    } else {
+        response.setHeader("Content-Type", "image/jpeg");
+        response.statusCode = 404;
+        fs.readFile('./404.jpg', function (err, data) {
+            response.write(data);
+            response.end();
+        })
+    }
+});
+
+server.listen(8080);
